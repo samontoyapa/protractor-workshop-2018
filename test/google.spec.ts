2b24ce7d06194fd9a8565d84629fd70fbe3ccd60
Santiago <samontoyapa@unal.edu.co>
2019-10-31 19:05:27 -0500

    Improve test (#2)
    * setup protractor configuration
    
    * improving protractor

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -1,5 +1,14 @@
 import { browser } from 'protractor';
 
+describe('Given a SDET learning protractor', () => {
+  describe('when open Google Page', () => {
+    beforeEach(() => {
+      browser.get('http://www.google.com');
+    });
+
+    it('then should have a title', () => {
+      expect(browser.getTitle()).toEqual('Google');
+    });
 describe('This is the first example of protractor', () => {
   it('should have a title', () => {
     browser.ignoreSynchronization = true;
